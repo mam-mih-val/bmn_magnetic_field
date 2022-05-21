d5a58b7d02e6fb316916ae301751e3febae39d29
mam-mih-val <mam.mih.val@gmail.com>
2022-05-21 23:07:27 +0300

    Saving the result to PDF-files

--- macro/draw_2dgraph.cc
+++ macro/draw_2dgraph.cc
@@ -4,11 +4,14 @@
 
 void draw_2dgraph(){
   // The vector of input file names
-  std::vector<std::string> v_in_file_names{"/home/mikhail/bmn_magnetic_field/data/2022x05x20_1900A_hall_93_CW.csv",};
+  std::vector<std::string> v_in_file_names{
+      "/home/mikhail/bmn_magnetic_field/data/2022x05x21_1900A_hall_91_CW.csv",
+      "/home/mikhail/bmn_magnetic_field/data/2022x05x21_1900A_hall_91_part2_CW.csv"
+  };
   // The vector of start positions of the measuring head
-  std::vector<double> v_start_y{0.0, 1100};
+  std::vector<double> v_start_y{0.0, 5050};
   // The output file will be named as follows
-  std::string out_file_name = "2022x05x20_1900A_hall_93_CW.root";
+  std::string out_file_name = "2022x05x21_1900A_hall_91_CW";
 
   // Parsing the input files
   std::vector<FieldPlane> planes;
@@ -34,46 +37,69 @@ void draw_2dgraph(){
 
   plane = plane / 1'000;
 
-  auto file_out = TFile::Open( out_file_name.c_str(), "recreate" );
+  auto file_out = TFile::Open( std::data(out_file_name+".root"), "recreate" );
 
   // Creating the 2D plots of the field components
-  auto slice_plot_bx = plane.GetPlaneGraph( "Bx_vs_x_y", ";x (mm); y (mm); Bx (kGs)", bx_field() );
-  auto slice_histo_bx = plane.GetPlaneHisto( "h2_Bx_vs_x_y", ";x (mm); y (mm); B_{x} (kGs)", bx_field() );
-  auto slice_plot_by = plane.GetPlaneGraph( "By_vs_x_y", ";x (mm); y (mm); By (kGs)", by_field() );
-  auto slice_histo_by = plane.GetPlaneHisto( "h2_By_vs_x_y", ";x (mm); y (mm); By (kGs)", by_field() );
-  auto slice_plot_bz = plane.GetPlaneGraph( "Bz_vs_x_y", ";x (mm); y (mm); Bz (kGs)", bz_field() );
-  auto slice_histo_bz = plane.GetPlaneHisto( "h2_Bz_vs_x_y", ";x (mm); y (mm); Bz (kGs)", bz_field() );
+  auto slice_histo_bx = plane.GetPlaneHisto( "h2_Bx_vs_x_y", "Bx;x (mm); y (mm); B_{x} (kGs)", bx_field() );
+  auto slice_histo_by = plane.GetPlaneHisto( "h2_By_vs_x_y", "By;x (mm); y (mm); By (kGs)", by_field() );
+  auto slice_histo_bz = plane.GetPlaneHisto( "h2_Bz_vs_x_y", "Bz;x (mm); y (mm); Bz (kGs)", bz_field() );
 
-  double y=0;
   // Creating the directories in output file for each field component to write 1D projection to
   file_out->mkdir( "Bx" );
   file_out->mkdir( "By" );
   file_out->mkdir( "Bz" );
   double mean_x = 0;
   double n_entries = 0;
-  while( y< 4674.0 ){
+  std::vector<TCanvas*> canvases;
+  gStyle->SetOptStat(0);
+  gStyle->SetPalette(kViridis);
+  canvases.emplace_back( new TCanvas( "2D_pictures", "", 1920, 1080 ) );
+  canvases.back()->cd();
+  canvases.back()->Divide(3, 1);
+  canvases.back()->cd(1);
+  slice_histo_bx->Draw("colz");
+  canvases.back()->cd(2);
+  slice_histo_by->Draw("colz");
+  canvases.back()->cd(3);
+  slice_histo_bz->Draw("colz");
+  canvases.back()->SaveAs(std::data(out_file_name+".pdf("));
+  auto unique_y_coordinates = plane.GetUniqueYCoordinates();
+  int i=0;
+  for( auto y : unique_y_coordinates ){
     // Getting the vector of points with current y coordinate with tolerance of 10 mm (because '==' comparison doesn't make sence for doubles)
     auto slice = FieldPlane(plane.SelectPoints( y_equals{y, 10} ));
     // Converting the points into drawable TGraph
     auto int_y = int(y);
-    auto proj_bx = slice.GetGraph( "y_"+std::to_string(int_y), ";x (mm); B_{x} (kGs)", x_coordinate(), bx_field() );
-    auto proj_by = slice.GetGraph( "y_"+std::to_string(int_y), ";x (mm); B_{y} (kGs)", x_coordinate(), by_field() );
-    auto proj_bz = slice.GetGraph( "y_"+std::to_string(int_y), ";x (mm); B_{z} (kGs)", x_coordinate(), bz_field() );
+    auto proj_bx = slice.GetGraph( "y_"+std::to_string(int_y), "B_{x}", x_coordinate(), bx_field() );
+    proj_bx->SetLineColor( kBlue );
+    auto proj_by = slice.GetGraph( "y_"+std::to_string(int_y), "B_{y}", x_coordinate(), by_field() );
+    proj_by->SetLineColor( kRed );
+    auto proj_bz = slice.GetGraph( "y_"+std::to_string(int_y), "B_{z}", x_coordinate(), bz_field() );
+    proj_bz->SetLineColor( kGreen+2 );
+    auto stack = new TMultiGraph();
+    stack->SetTitle( std::data("y="+std::to_string(int_y)+" mm;x (mm); B_{i} (kGs)") );
+    stack->Add( proj_bx, "l" );
+    stack->Add( proj_by, "l" );
+    stack->Add( proj_bz, "l" );
+    canvases.emplace_back( new TCanvas( std::data("y_"+std::to_string(int_y)), "", 1920, 1080 ) );
+    canvases.back()->cd();
+    stack->Draw( "al" );
+    stack->GetHistogram()->SetMinimum( -6 );
+    stack->GetHistogram()->SetMaximum( 15 );
+    stack->Draw( "al" );
+    canvases.back()->BuildLegend( 0.1, 0.75, 0.3, 0.95 );
+    std::string save_name = i == unique_y_coordinates.size() - 1 ? out_file_name+".pdf)" : out_file_name+".pdf";
+    canvases.back()->SaveAs(save_name.c_str());
+
     file_out->cd( "Bx" );
     proj_bx->Write();
     file_out->cd( "By" );
     proj_by->Write();
     file_out->cd( "Bz" );
     proj_bz->Write();
-
-    // The step of the measurement on the y-axis
-    y+=25;
+    i++;
   }
-
   file_out->cd();
-  slice_plot_bx->Write();
-  slice_plot_by->Write();
-  slice_plot_bz->Write();
   slice_histo_bx->Write();
   slice_histo_by->Write();
   slice_histo_bz->Write();
